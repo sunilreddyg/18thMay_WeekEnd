bd567d88513acc9565a76c760ebdb961557c524c
SUNIL <SUNIL@192.168.1.3>
2019-06-30 12:52:25 +0530

    Updation of first project

---
+++ AtlasBank/src/test/java/p1/MyTest1.java
@@ -0,0 +1,18 @@
+package p1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MyTest1 {
+  @Test
+  public void tc001()
+  {
+	  Reporter.log("tc001 executed",true);
+  }
+  
+  @Test
+  public void tc002()
+  {
+	  Reporter.log("tc002 executed",true);
+  }
+}
---
+++ AtlasBank/src/test/java/p2/MyTest2.java
@@ -0,0 +1,24 @@
+package p2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MyTest2 {
+  @Test
+  public void tc003()
+  {
+	  Reporter.log("tc003 executed",true);
+  }
+  
+  @Test
+  public void tc004()
+  {
+	  Reporter.log("tc004 executed",true);
+  }
+
+  @Test
+  public void tc005()
+  {
+	  Reporter.log("tc005 executed",true);
+  }
+}
